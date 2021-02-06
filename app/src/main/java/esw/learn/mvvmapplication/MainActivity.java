82da9e976cd85dc1a1092c1a159f485384e9d4bf
Scarecrow94 <adesuyiayo@rocketmail.com>
2021-02-06 21:15:36 +0100

    Project initialization

---
+++ app/src/main/java/esw/learn/mvvmapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package esw.learn.mvvmapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
